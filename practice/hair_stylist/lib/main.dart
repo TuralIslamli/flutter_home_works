9136afc882458480d7d10dab658730353685b09c
Tural Islamli <tural.islamli@technofusion.az>
2023-07-20 13:23:07 +0400

    Hair stylist

--- practice/hair_stylist/lib/main.dart
+++ practice/hair_stylist/lib/main.dart
@@ -9,12 +9,277 @@ class HairStylist extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: Text("Hello world!"),
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 248, 243, 225),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          iconTheme: IconThemeData(color: Colors.black),
+          elevation: 0,
+        ),
+      ),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.arrow_back_ios_new_rounded)),
+            actions: [
+              IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.search_rounded))
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              const SizedBox(height: 10),
+              const Text(
+                "Hair Stylist",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 250,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Icon(
+                              Icons.face_retouching_natural,
+                              size: 100,
+                            ),
+                            const Text(
+                              "Anna Ray",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Icon(
+                                  Icons.star_rounded,
+                                  color: Colors.yellow,
+                                  size: 20,
+                                ),
+                                Icon(
+                                  Icons.star_rounded,
+                                  color: Colors.yellow,
+                                  size: 20,
+                                ),
+                                Icon(
+                                  Icons.star_rounded,
+                                  color: Colors.yellow,
+                                  size: 20,
+                                ),
+                                Icon(
+                                  Icons.star_rounded,
+                                  color: Colors.yellow,
+                                  size: 20,
+                                ),
+                                Icon(
+                                  Icons.star_rounded,
+                                  color: Colors.yellow,
+                                  size: 20,
+                                ),
+                              ],
+                            ),
+                            const Text(
+                              "5 years experience",
+                              style: TextStyle(
+                                fontSize: 10,
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.phone,
+                                    size: 20,
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.email,
+                                    size: 20,
+                                  ),
+                                )
+                              ],
+                            )
+                          ]),
+                    ),
+                  ),
+                  Container(
+                    width: 150,
+                    height: 250,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Icon(
+                              Icons.face_outlined,
+                              size: 100,
+                            ),
+                            const Text(
+                              "George Sebo",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Icon(
+                                  Icons.star_rounded,
+                                  color: Colors.yellow,
+                                  size: 20,
+                                ),
+                                Icon(
+                                  Icons.star_rounded,
+                                  color: Colors.yellow,
+                                  size: 20,
+                                ),
+                                Icon(
+                                  Icons.star_rounded,
+                                  color: Colors.yellow,
+                                  size: 20,
+                                ),
+                                Icon(
+                                  Icons.star_rounded,
+                                  color: Colors.yellow,
+                                  size: 20,
+                                ),
+                                Icon(
+                                  Icons.star_rounded,
+                                  size: 20,
+                                ),
+                              ],
+                            ),
+                            const Text(
+                              "4 years experience",
+                              style: TextStyle(
+                                fontSize: 10,
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.phone,
+                                    size: 20,
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.email,
+                                    size: 20,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ]),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    "Schedule",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "Sept, 2020",
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 12,
+                      horizontal: 18,
+                    ),
+                    child: Column(
+                      children: const [
+                        Text(
+                          "11",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "S",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Stack(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(3),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: const Color.fromARGB(255, 65, 32, 32),
+                          ),
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 12,
+                            horizontal: 18,
+                          ),
+                          child: Column(
+                            children: const [],
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ]),
           ),
         ),
       ),
